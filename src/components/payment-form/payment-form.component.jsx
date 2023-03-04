d930b403b997565ff1c141272755b0dcb69994c6
PrAnAvViNaYaKjAdHaV <pj8841466@gmail.com>
2023-03-05 00:27:36 +0530

    phone number is complete

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,55 +13,58 @@ const PaymentForm = () => {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
   const User = useSelector(selectCurrentUser);
-  const [fromName, setFormName] = useState();
-  const [phoneNumber, setFornumber] = useState();
+  const [fromName, setFormName] = useState(null);
+  const [phoneNumber, setFornumber] = useState(null);
   const [item, setItem] = useState(cartItems);
   const [total, setTotal] = useState();
-  const [address, setAddress] = useState();
+  const [address, setAddress] = useState(null);
   const dispatch = useDispatch();
   useEffect(() => {
     const items = cartItems.map((data) => [data.name + "=" + data.quantity]);
     setItem(items.toString());
     setTotal(cartTotal.toString());
   }, [cartTotal]);
+  console.log(fromName, phoneNumber, item, total, address);
   function sendEmail(e) {
+    e.preventDefault();
     if (
       phoneNumber === null ||
-      item === null ||
-      total === null ||
+      item === [] ||
+      total === 0 ||
       User === null ||
       address === null
     ) {
+      console.log("return");
       return;
+    } else {
+      const emailConstant = {
+        from_name: fromName,
+        from_email: User.email,
+        item: item,
+        total: total,
+        address: address,
+        from_number: phoneNumber,
+      };
+      emailjs
+        .send(
+          "service_s638xb8",
+          "template_iwmj80t",
+          emailConstant,
+          "vFmkgEt53JE1uTz0L"
+        )
+        .then(
+          (result) => {
+            window.location.reload();
+            dispatch(orderComplete());
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
     }
-    e.preventDefault();
-    const emailConstant = {
-      from_name: fromName,
-      from_email: User.email,
-      item: item,
-      total: total,
-      address: address,
-      from_number: phoneNumber,
-    };
-    emailjs
-      .send(
-        "service_s638xb8",
-        "template_iwmj80t",
-        emailConstant,
-        "vFmkgEt53JE1uTz0L"
-      )
-      .then(
-        (result) => {
-          window.location.reload();
-          dispatch(orderComplete());
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
   }
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={(e) => sendEmail(e)}>
       <input type="hidden" name="contact_number" />
       <div>
         <label>Name</label>
@@ -69,15 +72,38 @@ const PaymentForm = () => {
           type="text"
           name="from_name"
           onChange={(e) => setFormName(e.target.value)}
+          required
         />
       </div>
       <div>
         <label>Phone Number</label>
         <input
           type="number"
+          id="phone"
           name="from_number"
           onChange={(e) => setFornumber(e.target.value)}
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          size="10"
+          required
         />
+        {phoneNumber ? (
+          phoneNumber.length < 10 ? (
+            <p className="warning ">Phonenumber cant be less than 10</p>
+          ) : (
+            ""
+          )
+        ) : (
+          ""
+        )}
+        {phoneNumber ? (
+          phoneNumber.length > 10 ? (
+            <p className="warning ">Phonenumber cant be greater than 10</p>
+          ) : (
+            ""
+          )
+        ) : (
+          ""
+        )}
       </div>
       <div>
         <label>Address</label>
@@ -87,7 +113,7 @@ const PaymentForm = () => {
       <input
         type="submit"
         value="Order"
-        onClick={(e) => sendEmail(e)}
+        // onClick={(e) => sendEmail(e)}
         style={{
           background: User === null || total === "0" ? "gray" : "green",
         }}
